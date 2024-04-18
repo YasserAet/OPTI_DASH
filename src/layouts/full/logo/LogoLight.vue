fc4a132bd2d12cbf90aa046fbdb04b973d276477
YasserAet <yasseraitali@outlook.fr>
2024-04-18 12:35:24 +0100

    dash imported

---
+++ src/layouts/full/logo/LogoLight.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
+const primary = ref('rgb(var(--v-theme-primary))');
+const secondary = ref('rgb(var(--v-theme-secondary))');
+</script>
+<template>
+    <div class="logo">
+        <RouterLink to="/">
+            <img src="src\assets\images\logos\logo.svg" alt="logo" />
+
+            
+        </RouterLink>
+    </div>
+</template>
